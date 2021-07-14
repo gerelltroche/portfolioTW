a064da116d2221290f80f6cc4338759d5aa13eb0
tuliotroche <tgtroche@gmail.com>
2021-07-14 12:31:06 -0400

    Responsiveness Changes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -8,7 +8,9 @@ const Hero = () => (
             <h1>Hello, I'm <strong className={'text-orange-500'}>Gerell Troche</strong></h1>
             <h1 className={'mb-6'}>I'm a web developer.</h1>
             <button
-                className="transition bg-blue-500 hover:bg-blue-400 text-gray-50 font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:text-white rounded">
+                className="transition bg-blue-500 hover:bg-blue-400 text-gray-50 font-bold py-2 px-4
+                border-b-4 border-blue-700 hover:border-blue-500 hover:text-white rounded"
+            >
                 <div className={''}>my work</div>
             </button>
         </div>
